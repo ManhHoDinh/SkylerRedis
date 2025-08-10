964d3daadb43552a224e133b44942a214769d180
ManhHoDinh <21522327@gm.uit.edu.vn>
2025-08-10 12:31:42 +0700

    Send handshake 2/3

--- app/main.go
+++ app/main.go
@@ -67,8 +67,8 @@ func sendToMaster(){
 				conn.Close()
 			}
 			fmt.Println("Sent PING to master")
-
-			conn.Write([]byte(fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%s\r\n", *port)))
+			time.Sleep(30 * time.Millisecond)
+			_, err = conn.Write([]byte(fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%s\r\n", *port)))
 
 			if err != nil {
 				fmt.Println("Connection to master lost:", err)
