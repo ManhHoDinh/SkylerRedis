17049c95e031db4db83b09d988122b4cf00503c9
Manh Ho Dinh <HodinhM@danieli.it>
2025-08-11 10:22:29 +0700

    Empty RDB transfer

--- app/command/psync.go
+++ app/command/psync.go
@@ -3,8 +3,12 @@ package command
 import (
 	"SkylerRedis/app/utils"
 	"net"
+	"time"
 )
 
 func handlePSYNC(conn net.Conn, args []string) {
 	utils.WriteSimpleString(conn, "FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0")
+	time.Sleep(5 * time.Millisecond)
+	utils.WriteBulkString(conn, "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==")
+	time.Sleep(5 * time.Millisecond)
 }
--- app/handler/handler.go
+++ app/handler/handler.go
@@ -2,15 +2,17 @@ package handler
 
 import (
 	"SkylerRedis/app/command"
-	"SkylerRedis/app/utils"
 	"SkylerRedis/app/server"
+	"SkylerRedis/app/utils"
 	"bufio"
+	"fmt"
 	"net"
 )
 
 func HandleConnection(conn net.Conn, server server.Server) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
+	fmt.Println("Received data: ", reader)
 	for {
 		args, err := utils.ParseArgs(conn, reader)
 		if err != nil {
