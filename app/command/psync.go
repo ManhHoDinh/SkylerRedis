d305ccad4294787674a72dd9064836382424b494
Manh Ho Dinh <HodinhM@danieli.it>
2025-08-13 16:55:12 +0700

    change to single gorouting

--- app/command/psync.go
+++ app/command/psync.go
@@ -3,35 +3,10 @@ package command
 import (
 	"SkylerRedis/app/server"
 	"SkylerRedis/app/utils"
-	"bufio"
-	"fmt"
-	"io"
 )
 
 func handlePSYNC(server server.Server, args []string) {
-	// 1. Acknowledge FULLRESYNC
 	utils.WriteSimpleString(server.Conn, "FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0")
-
-	// 2. Read the RDB bulk string header: $<len>
-	lenLine, _ := bufio.NewReader(server.Conn).ReadString('\n')
-	var rdbLen int
-	fmt.Sscanf(lenLine, "$%d", &rdbLen)
-
-	// 3. Read and discard the RDB content
-	buf := make([]byte, rdbLen)
-	io.ReadFull(server.Conn, buf)
-
-	// 4. Now enter loop to read propagated commands from master
-	for {
-		args, err := utils.ParseArgs(server.Conn, bufio.NewReader(server.Conn))
-		if err != nil {
-			fmt.Println("Error reading propagated command:", err)
-			return
-		}
-		if len(args) == 0 {
-			continue
-		}
-		// Apply to our DB, but DO NOT send any reply to master
-		HandleCommand(server, args)
-	}
+	//RDBcontent, _ := hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
+	//server.Conn.Write([]byte(fmt.Sprintf("$%v\r\n%v", len(string(RDBcontent)), string(RDBcontent))))
 }
